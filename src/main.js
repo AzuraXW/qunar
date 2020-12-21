c8542482d4a812815e6573e4786bfca14510e2ae
wangceng <2219444825@qq.com>
2020-12-21 18:53:51 +0800

    init ready

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router'
 
 Vue.config.productionTip = false
 
+// 初始化样式
+import 'normalize.css/normalize.css'
+// fastclick
+import fastClick from 'fastclick'
+fastClick.attach(document.body)
+
+Vue.use(VueRouter)
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+import VueRouter from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
+
+export default new VueRouter({
+    path: '/',
+    component: HelloWorld
+})
